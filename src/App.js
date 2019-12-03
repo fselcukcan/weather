78a43eaf9d805cf8736d764cffecc18d9b1a29d3
Selçuk Can <selcuk@Selcuks-MacBook-Pro-2.local>
2019-12-03 19:40:56 +0300

    added new favicon and manifest file icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './weather-ios-logo-svg-vector.svg';
+import logo from './logo.svg';
 import './App.css';
 import { Tabs } from "@bumaga/tabs";
 import config from "./config";
