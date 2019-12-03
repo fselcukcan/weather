4aece495af05ba2adefc94fc8cf40d5ae5e89781
Selçuk Can <selcuk@Selcuks-MacBook-Pro-2.local>
2019-12-03 19:41:59 +0300

    added build folder for github pages

---
+++ build/precache-manifest.1759f7b96f078cd1f8dfebe35f88f3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14de3931d14ea06276a54161354101ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cd3e83134484c212ab",
+    "url": "/static/css/main.e976e446.chunk.css"
+  },
+  {
+    "revision": "b0cbdbaf6cf94672e94e",
+    "url": "/static/js/2.dfaeddc5.chunk.js"
+  },
+  {
+    "revision": "99cd3e83134484c212ab",
+    "url": "/static/js/main.a2ce125f.chunk.js"
+  },
+  {
+    "revision": "ebfe78bb80b70084c45b",
+    "url": "/static/js/runtime-main.5de5fce0.js"
+  },
+  {
+    "revision": "658f99fdeb332cbdc2a56fb43b0a0b96",
+    "url": "/static/media/logo.658f99fd.svg"
+  }
+]);
