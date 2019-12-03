68ff4a37d78633be8fb2364a0dd693cac6ad477f
Selçuk Can <selcuk@Selcuks-MacBook-Pro-2.local>
2019-12-03 19:54:28 +0300

    Updates

---
+++ precache-manifest.a104f1a4c17584c29a95cfa7d06b5ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ae07c73b8c17e356aa7aa5cacaf758",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ac986f2106476d41a839",
+    "url": "/weather/static/css/main.e976e446.chunk.css"
+  },
+  {
+    "revision": "b0cbdbaf6cf94672e94e",
+    "url": "/weather/static/js/2.dfaeddc5.chunk.js"
+  },
+  {
+    "revision": "ac986f2106476d41a839",
+    "url": "/weather/static/js/main.9892124b.chunk.js"
+  },
+  {
+    "revision": "a439d785d83b9852b72f",
+    "url": "/weather/static/js/runtime-main.2412ce67.js"
+  },
+  {
+    "revision": "658f99fdeb332cbdc2a56fb43b0a0b96",
+    "url": "/weather/static/media/logo.658f99fd.svg"
+  }
+]);
